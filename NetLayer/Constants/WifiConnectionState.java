0c676f9c8f80153d9263f5a0c2c6085536c7dee2
Felipe Valverde <felipe.valverde.campos@gmail.com>
2012-05-21 20:49:53 -0400

    From https://github.com/fvalverd/High-Level-MANET-Protocol/tree/java/source/java/hlmp

---
+++ NetLayer/Constants/WifiConnectionState.java
@@ -0,0 +1,8 @@
+package hlmp.NetLayer.Constants;
+
+public final class WifiConnectionState {
+	public static final int CONNECTED = 1;
+    public static final int FAILED = 2;
+    public static final int WAITING = 4;
+    public static final int STOP = 5;
+}
